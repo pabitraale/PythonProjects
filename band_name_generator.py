a1b23265168f70c4c1460008faf17de055a24955
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-09-22 00:48:06 +0000

    added band name generator script.

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city + " " + pet_name
+print("Your band name could be "+ band_name)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Write your code below this line 👇
-print("Hello, World!!")
-print("Hello, How are you?")
-print("Hello, World!!")
-print("Hello, How are you?")
+
+a = int(input("a: "))
+print(type(a))
