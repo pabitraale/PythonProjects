dd062da86f63e90501a1b3d004d68c48178d0f8f
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-09-26 19:32:40 +0000

    added extra code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -26,32 +26,35 @@ scissors = '''
 ---.__(___)
 '''
 your_num = int(input("What do you choose? Type 0 for Rock, 1, for Paper or 2 for Scissors.\n"))
-if your_num == 0:
-  print(rock)
-elif your_num == 1:
-  print(paper)
+if your_num >2 or your_num < 0:
+    print("Invalid input. You lose")
 else:
-  print(scissors)
+    if your_num == 0:
+        print(rock)
+    elif your_num == 1:
+        print(paper)
+    else:
+        print(scissors)
 
-computer_num = random.randint(0,2)
-print("Computer chose: ")
+    computer_num = random.randint(0,2)
+    print("Computer chose: ")
 
-if computer_num == 0:
-  print(rock)
-elif computer_num == 1:
-  print(paper)
-else:
-  print(scissors)
+    if computer_num == 0:
+        print(rock)
+    elif computer_num == 1:
+        print(paper)
+    else:
+        print(scissors)
 
-if your_num > 2 or your_num < 0:
-    print("Invalid input.")
-elif your_num == 0 and computer_num == 2:
-    print("You win!")
-elif your_num == 2 and computer_num == 0:
-    print("You lose")
-elif your_num < computer_num:
-    print("You lose")
-elif your_num > computer_num:
-    print("You win!")
-elif your_num == computer_num:
-    print("It's a draw.")
+    if your_num > 2 or your_num < 0:
+        print("Invalid input. You lose")
+    elif your_num == 0 and computer_num == 2:
+        print("You win!")
+    elif your_num == 2 and computer_num == 0:
+        print("You lose")
+    elif your_num < computer_num:
+        print("You lose")
+    elif your_num > computer_num:
+        print("You win!")
+    elif your_num == computer_num:
+        print("It's a draw.")
