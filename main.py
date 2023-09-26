1185055b9adb2d39aee6770317b136318107e014
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-09-26 19:22:50 +0000

    rock paper scissors game added

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 # Write your code below this line 👇
 
-height = float(input("What is your height in m: "))
-weight = float(input("What is your weight in kg: "))
+# import random
 
-bmi = weight/ (height*height)
-print(bmi)
+# random_int = random.randint(1, 10)
+# print(random_int)
+
+# random_float = random.random()
+# print(random_float)
+
+# states_of_america = ["Delaware", "Pennsylvania", "Ohio"]
+# print(states_of_america[-1])
+
+# names_str = input("write names: ")
+# print(names_str)
+# print(type(names_str))
+
+# names = names_str.split(",")
+# fruits = ["apple", "banana", "grape"]
+# vegs = ["pototes", "spinach"]
+# fruits_and_vegs = [fruits, vegs]
+# print(fruits_and_vegs)
+# print(fruits_and_vegs[1][1])
+
+# print(names)
+# print(type(names))
+
+
+
