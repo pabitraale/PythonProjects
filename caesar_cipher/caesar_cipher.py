9313a3d6d95db06a0bf45577393652ca1746dd42
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-19 20:47:10 +0000

    edited code

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -3,28 +3,16 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 message = input("What is your message: \n").lower()
 shift_number = int(input("What number you want to shift:\n "))
-#encrypt function
-def encrypt(text, number):
-    encrypted_word=""
+
+def caesar(text, shift_num, cipher_direction):
+   
+    final_word=""
     for letter in text:
         letter_index = alphabet.index(letter)
-        shift_num = letter_index + number
-        encrypted_word += alphabet[shift_num]
-    print("The encoded text is: ",encrypted_word)
-
-#decrypt function
-def decrypt(encrypted_text, number):
-    decrypted_text =""
-    for letter in encrypted_text:
-        letter_index = alphabet.index(letter)
-        shift_num = letter_index - number
-        decrypted_text += alphabet[shift_num]
-            
-    print("The decoded text is: ", decrypted_text)
-
-
-if direction == "encode":
-    encrypt(message, shift_number)
-elif direction == "decode":
-    decrypt(message, shift_number)
+        if cipher_direction == "decode":
+            shift_num *= -1
+        new_letter_index = letter_index + shift_num
+        final_word += alphabet[new_letter_index]
+    print(f"The {cipher_direction}d text is {final_word}") 
     
+caesar(message, shift_number, direction)        
