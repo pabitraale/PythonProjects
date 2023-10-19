567036b9f7e435f5432b357c6483399f67bee5ea
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-19 20:58:51 +0000

    added logo

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,3 +1,7 @@
+import art
+logo = art.logo
+
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 #user input
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
@@ -7,10 +11,10 @@ shift_number = int(input("What number you want to shift:\n "))
 def caesar(text, shift_num, cipher_direction):
    
     final_word=""
+    if cipher_direction == "decode":
+        shift_num *= -1
     for letter in text:
         letter_index = alphabet.index(letter)
-        if cipher_direction == "decode":
-            shift_num *= -1
         new_letter_index = letter_index + shift_num
         final_word += alphabet[new_letter_index]
     print(f"The {cipher_direction}d text is {final_word}") 
