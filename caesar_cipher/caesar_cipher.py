52cd5113a1f5be407d06b989829dcc5233916e74
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-19 19:55:45 +0000

    added encrypt function

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,6 +1,22 @@
-encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-print(encode_or_decode)
-message = input("What is your message: ").lower()
-print(message)
-shift_number = int(input("What number you want to shift: "))
-print(shift_number)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#user input
+encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").lower()
+message = input("What is your message: \n").lower()
+shift_number = int(input("What number you want to shift:\n "))
+#encrypt 
+def encrypt(text, number):
+    encrypted_word=""
+    for letter in text:
+        letter_index = alphabet.index(letter)
+        shift_num = letter_index + number
+        if shift_num > 25:
+            extra_num = shift_num - 26
+            encrypted_word += alphabet[extra_num]
+        else:
+            encrypted_word += alphabet[shift_num]
+    print("The encoded text is: ",encrypted_word)
+
+encrypt(message, shift_number)
+
+
+
--- main.py
+++ main.py
@@ -29,104 +29,21 @@
 # for fruit in fruits:
 #   print(fruit)
 # print(fruit)
-import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["ardvark", "baboon", "camel"]
+# def greet(name):
+#     print(f"Hi,{name} how are you?")
 
-#Get random number from the list
-rand_num = random.randint(0,len(word_list)-1)
-#Use that random numbert to pick the word from the list
-chosen_word = word_list[rand_num]
-print(chosen_word)
-lives = 6
+# greet("Sophie")
 
-display = []
-# Create list having _ for each letter in the word
-for _ in range(len(chosen_word)):
-        display += "_"
-# Loop until word is complete
-end_game = False
-while not end_game:
-    #Ask user to enter the guess letter
-    guess = input("Guess a letter: ").lower()
-    #check if guess letter in the chosen word or not
-    #If guessed letter in the chosen word than 
-    #replace the _ with the guess letter
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            display[i] = guess
-            #chosen_word_length -= 1
+def greet_with(name, location):
+    print(f"I am {name}, I am from {location}.")
+greet_with(location ="Kathmandu", name="Sophie")
 
-    if guess not in chosen_word:
-        lives -= 1
-        if lives == 0:
-            end_game = True
-            print("You lose.")
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
 
-    print(f"{' '.join(display)}")
-    #check if display is complete or not
-    if "_" not in display:
-        end_game = True
-        print("You win.")
-
-    print(stages[lives])
-        
-
-  
 
 
