1a04d42fc5bdf8c6f4c31a71662070583d99905b
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-19 20:28:07 +0000

    added decrypted function

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,22 +1,30 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 #user input
-encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").lower()
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 message = input("What is your message: \n").lower()
 shift_number = int(input("What number you want to shift:\n "))
-#encrypt 
+#encrypt function
 def encrypt(text, number):
     encrypted_word=""
     for letter in text:
         letter_index = alphabet.index(letter)
         shift_num = letter_index + number
-        if shift_num > 25:
-            extra_num = shift_num - 26
-            encrypted_word += alphabet[extra_num]
-        else:
-            encrypted_word += alphabet[shift_num]
+        encrypted_word += alphabet[shift_num]
     print("The encoded text is: ",encrypted_word)
 
-encrypt(message, shift_number)
+#decrypt function
+def decrypt(encrypted_text, number):
+    decrypted_text =""
+    for letter in encrypted_text:
+        letter_index = alphabet.index(letter)
+        shift_num = letter_index - number
+        decrypted_text += alphabet[shift_num]
+            
+    print("The decoded text is: ", decrypted_text)
 
 
-
+if direction == "encode":
+    encrypt(message, shift_number)
+elif direction == "decode":
+    decrypt(message, shift_number)
+    
