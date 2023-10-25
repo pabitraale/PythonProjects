af79b5cefe6b0af2555c8dc9c2b205203b585e36
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-25 17:25:09 +0000

    added while loop

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -7,6 +7,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 message = input("What is your message: \n").lower()
 shift_number = int(input("What number you want to shift:\n "))
+shift_number = shift_number % 26
 
 def caesar(text, shift_num, cipher_direction):
    
@@ -14,9 +15,33 @@ def caesar(text, shift_num, cipher_direction):
     if cipher_direction == "decode":
         shift_num *= -1
     for letter in text:
-        letter_index = alphabet.index(letter)
-        new_letter_index = letter_index + shift_num
-        final_word += alphabet[new_letter_index]
-    print(f"The {cipher_direction}d text is {final_word}") 
+        if letter in alphabet:
+            letter_index = alphabet.index(letter)
+            new_letter_index = letter_index + shift_num
+            final_word += alphabet[new_letter_index]
+        else:
+            final_word += letter
+        
+    print(f"The {cipher_direction}d text is: {final_word}") 
     
-caesar(message, shift_number, direction)        
+caesar(message, shift_number, direction)  
+#if user want to play again or not
+
+boolean_flag = True
+while boolean_flag:
+    play_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if play_again == 'no'.lower():
+        boolean_flag = False
+        print("Goodbye")
+        break
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    message = input("What is your message: \n").lower()
+    shift_number = int(input("What number you want to shift:\n "))
+    shift_number = shift_number % 26
+    
+    caesar(message, shift_number, direction)
+    
+        
+        
+
+    
