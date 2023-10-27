0df55756f027920ddac81991b93af56d0c139ed4
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-10-27 17:11:58 +0000

    added blind auction script

---
+++ blind_auction_program/blind_auction.py
@@ -0,0 +1,45 @@
+from replit import clear
+import art
+
+logo = art.logo
+print(logo)
+
+print("Welcome to the secret auction program.")
+bidder = {}
+#find highest bidder
+def find_highest_bidder(bidder_dict):
+    maxVal = 0
+    bidder_name = ""
+    for key in bidder_dict:
+        if maxVal < bidder_dict[key]:
+            maxVal = bidder_dict[key]
+            bidder_name = key
+    return bidder_name, maxVal
+
+
+boolean_flag = True
+
+while boolean_flag:
+    name = input("What is your name?:")
+    bid_amt = int(input("What's your bid?: $"))
+    bidder[name] = bid_amt
+    print(bidder)
+    other_bidder = input("Are there any other bidders? Type 'yes' or 'no'.")
+
+    if other_bidder == "yes":
+        clear()   
+    elif other_bidder == "no":
+        winner_name = find_highest_bidder(bidder)[0]
+        price = find_highest_bidder(bidder)[1]
+        print(f"The winner is {winner_name} with a bid of ${price}")
+        boolean_flag = False
+    else:
+        print("Invalid input. ")
+        other_bidder = input("Are there any other bidders? Type 'yes' or 'no'.")
+        clear()
+        
+
+        
+        
+
+
--- main.py
+++ main.py
@@ -34,16 +34,118 @@
 
 # greet("Sophie")
 
-def greet_with(name, location):
-    print(f"I am {name}, I am from {location}.")
-greet_with(location ="Kathmandu", name="Sophie")
 
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
+#Dictionaries
+#Nesting
+'''programming_dictionary = {
+    "Bug": "An error in the program from runnng as expected.",
+    "Function": "A piece of code that you can easily call over and over again",
+    "Loop": "The action of doing something over and over again,",}
+
+print(programming_dictionary["Bug"])
+# Adding new items to disctionary
+programming_dictionary["Compile Error"] = "This is an error."
+print(programming_dictionary)
+
+#create an empty dictionary
+empty_dictionary = {}
+#wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit an itme in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+#Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+#Nesting list and dictionaries
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany":["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#travel_log = {
+#    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visited": 12},
+#    "Germany":{"cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visited": 20},
+#}
+
+#print(travel_log["France"]["cities_visited"])
+
+#Nesting Dictionary in al list
+
+travel_log = [
+    {"country": "France", 
+     "cities_visited": ["Paris", "Lille", "Dijon"], 
+     "total_visited": 12
+    },
+    {"county": "Germany", 
+     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+     "total_visited": 20
+    },
+     ]
+print(travel_log[0]["cities_visited"])
 
 
+country = "Brazil" # Add country name
+visits = 2 # Number of visits
+list_of_cities = ["Sao Paulo", "Rio"] # create list from formatted string
 
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+def add_new_country(visited_country, visited, list_of_cities):
+    new_country = {}
+    new_country["country"] = visited_country 
+    new_country["visits"] = visited
+    new_country["cities"] =list_of_cities
+    
+    travel_log.append(new_country)
+        
+    #print(travel_log)
+
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+print(starting_dictionary)
+starting_dictionary["c"] = 7
+print(starting_dictionary)'''
+
+
+    
+        
+       
