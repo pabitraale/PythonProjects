4bafe63bd17882bb4da76e40871fa6c977e66c36
pabitraale <43688926+pabitraale@users.noreply.github.com>
2023-11-17 18:58:46 +0000

    added higher lower game script

---
+++ higher_lower_game/higher_lower.py
@@ -0,0 +1,57 @@
+from art import logo, vs
+import data
+import random
+from replit import clear
+
+print(logo)
+
+get_data = data.data
+#Generate one random data to start a game
+def get_random_data(data):
+    random_data = random.choice(get_data)
+    return random_data
+
+#compare the follower
+def compare_follower(data1_follower, data2_follower):
+    if data1_follower > data2_follower:
+        return "A".lower()
+    else:
+        return "B".lower()
+        
+right_score = 0
+wrong_score = 0
+data1 = get_random_data(get_data)  #get first data
+while wrong_score  < 1: 
+    print(f"Compare A: {data1['name']}, {data1['description']}, from {data1['country']}.")
+    print(vs)
+    
+    data2 = get_random_data(get_data)    
+    print(f"Against B: {data2['name']}, {data2['description']}, from {data2['country']}.")
+    
+    has_more_follower = compare_follower(data1['follower_count'], data2['follower_count'])
+    #print("more follower: ", has_more_follower)
+    #Ask the user who has more follower
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    #Compare whether user got score or not
+    if has_more_follower == "a" and user_choice == "a":
+        right_score += 1
+    elif has_more_follower == "a" and user_choice == "b":
+        wrong_score += 1
+    elif has_more_follower == "b" and user_choice == "a":
+        wrong_score += 1
+    else:
+        right_score += 1
+        
+    #Replace first data to second data
+    data1 = data2
+    clear()
+    print(logo)
+    #print score if user gives right and wrong answer
+    if wrong_score == 1:
+        print(f"Sorry, that's wrong. Final score:{right_score}")
+    else:
+        print(f"You're right! Current score: {right_score}")
+
+
+
+  
--- main.py
+++ main.py
@@ -276,7 +276,7 @@ if play_again == 'yes'.lower():
     clear()
     play_blackjack()'''
 
-import random
+'''import random
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 random_num = random.randint(1, 100)
@@ -306,10 +306,57 @@ while number_of_attempt > 0:
         print("Too low.")
         number_of_attempt -= 1
         print("Guess again.")
-        print(f"You have {number_of_attempt} attempts remaining to guess the number.")
+        print(f"You have {number_of_attempt} attempts remaining to guess the number.")'''
     
     
+# Day 13 debugging
+#Describe Problem
+'''def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+my_function()'''
+
+#Reproduce the bug
+'''from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_num)
+print(dice_imgs[dice_num])'''
+
+#Play Computer
+'''year = int(input("What's your year of birth? "))
+
+if  year >= 1994:
+    print("You are a Gen Z.")
+elif year < 1994 and year > 1980:
+    print("You are a millenial.")'''
+
+#fix the Errors
+'''age = int(input("How old are you? "))
+if age > 18:
+    print(f"You can drive at age {age}.")'''
+
+#Print is Your Friend
+'''pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)'''
+
+#Use a Debugger
+'''def mutate (a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+mutate([1,2,3,4,5,8,13])'''
+
+
     
+
     
     
 
